1f0481bf66ca24d90d220bfc1439b6426d34eaa6
Delta de Dirac <igorshevtsov@gmail.com>
2021-10-13 22:52:29 +0300

    add new campaign to the repository

--- src/main/java/com/mabaya/advertise/AdsController.java
+++ src/main/java/com/mabaya/advertise/AdsController.java
@@ -1,26 +1,27 @@
 package com.mabaya.advertise;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mabaya.advertise.entities.Campaign;
 import com.mabaya.advertise.entities.cust.ICampaignCust;
 import com.mabaya.advertise.entities.cust.IProductCust;
+import com.mabaya.advertise.entities.dto.CampaignDto;
 import com.mabaya.advertise.repos.CampaignRepo;
 import com.mabaya.advertise.repos.ProductRepo;
 
 @RestController
 public class AdsController {
+	@Autowired
+	private ModelMapper modelMapper;
 	private int daysActivePeriod = 10;
 	
 	private final ProductRepo productRepo;
@@ -63,9 +64,17 @@ public class AdsController {
 	}
 	
 	
-	@GetMapping("/createCampaign")
-	@ResponseBody
-	public String addCampaign(@RequestParam String name, @RequestParam int bid, @RequestParam String prodList, @RequestParam Timestamp startDate) {
-		return campaignRepo.save(new Campaign(name, bid, prodList, startDate)).toString();
+//	@GetMapping("/createCampaign")
+//	@ResponseBody
+//	public String addCampaign(@RequestParam String name, @RequestParam int bid, @RequestParam String prodList, @RequestParam Timestamp startDate) {
+//		return campaignRepo.save(new Campaign(name, bid, prodList, startDate)).toString();
+//	}
+	
+	@PostMapping("/createCampaign")
+	public Campaign addCampaign(@RequestBody CampaignDto newCampaignDto) {
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+		modelMapper.typeMap(CampaignDto.class, Campaign.class);
+		Campaign newCampaign = modelMapper.map(newCampaignDto, Campaign.class);
+		return campaignRepo.save(newCampaign);
 	}
 }
