21ce09998745d215195b5ec50f5e2ba6732a9cf4
Delta de Dirac <igorshevtsov@gmail.com>
2021-10-13 22:51:28 +0300

    add model mapper bean

--- src/main/java/com/mabaya/advertise/AdvertiseApplication.java
+++ src/main/java/com/mabaya/advertise/AdvertiseApplication.java
@@ -1,7 +1,9 @@
 package com.mabaya.advertise;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvertiseApplication {
@@ -10,4 +12,8 @@ public class AdvertiseApplication {
 		SpringApplication.run(AdvertiseApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
