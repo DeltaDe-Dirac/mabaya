0efb95571397be84e1cdd3e2a7b2ffab30202e97
Delta de Dirac <igorshevtsov@gmail.com>
2021-10-13 16:23:20 +0300

    query optimisation - return single value

--- src/main/java/com/mabaya/advertise/repos/CampaignRepo.java
+++ src/main/java/com/mabaya/advertise/repos/CampaignRepo.java
@@ -3,12 +3,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mabaya.advertise.entities.Campaign;
 import com.mabaya.advertise.entities.cust.ICampaignCust;
 
 public interface CampaignRepo extends JpaRepository<Campaign, Long> {
-	@Query(value = "SELECT C.* FROM CAMPAIGNS C WHERE DATEDIFF(day, C.START_DATE, CURDATE()) BETWEEN 0 AND ?1", nativeQuery = true)
-	List<ICampaignCust> getActiveCampaigns(int daysActivePeriod);
+	@Query(value = "SELECT C.* FROM CAMPAIGNS C WHERE DATEDIFF(day, C.START_DATE, CURDATE()) BETWEEN 0 AND :active_period ORDER BY BID DESC", nativeQuery = true)
+	List<ICampaignCust> getActiveCampaigns(@Param("active_period") int daysActivePeriod);
 }
 
--- src/main/java/com/mabaya/advertise/repos/ProductRepo.java
+++ src/main/java/com/mabaya/advertise/repos/ProductRepo.java
@@ -1,14 +1,17 @@
 package com.mabaya.advertise.repos;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.mabaya.advertise.entities.Product;
 import com.mabaya.advertise.entities.cust.IProductCust;
 
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
-	@Query(value = "SELECT P.* FROM PRODUCTS P WHERE P.CATEGORY=?1", nativeQuery = true)
-	List<IProductCust> getProductByCategory(String category);
+	@Query(value = "SELECT P.* FROM PRODUCTS P WHERE P.CATEGORY = :category AND P.ID IN :prod_list LIMIT 1", nativeQuery = true)
+	IProductCust getProductByCategory( @Param("category") String category, @Param("prod_list") String[] prodList);
+	
+	@Query(value = "SELECT P.* FROM PRODUCTS P WHERE P.ID IN :prod_list LIMIT 1", nativeQuery = true)
+	IProductCust getProductByIDs( @Param("prod_list") String[] prodList);
 }
 
